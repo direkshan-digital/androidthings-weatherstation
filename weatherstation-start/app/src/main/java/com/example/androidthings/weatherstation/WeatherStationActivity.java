bf9af82c1723761c79915f5114a6e94debdadd59
Kotecha, Prafulla <prafulla.kotecha@ingramcontent.com>
2017-09-26 17:24:34 -0400

    Finished codelab as part of droidconNYC 2017

--- weatherstation-start/app/src/main/java/com/example/androidthings/weatherstation/WeatherStationActivity.java
+++ weatherstation-start/app/src/main/java/com/example/androidthings/weatherstation/WeatherStationActivity.java
@@ -17,51 +17,165 @@
 package com.example.androidthings.weatherstation;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.things.contrib.driver.apa102.Apa102;
+import com.google.android.things.contrib.driver.bmx280.Bmx280SensorDriver;
+import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay;
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 public class WeatherStationActivity extends Activity {
     private static final String TAG = WeatherStationActivity.class.getSimpleName();
-
+    private AlphanumericDisplay mDisplay;
     // Default LED brightness
     private static final int LEDSTRIP_BRIGHTNESS = 1;
+    private Apa102 mLedstrip;
+    private Bmx280SensorDriver mEnvironmentalSensorDriver;
+    private SensorManager mSensorManager;
+
+    private SensorEventListener mSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            final float value = event.values[0];
+
+            if (event.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+                updateTemperatureDisplay(value);
+            }
+            if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
+                updateBarometerDisplay(value);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            Log.d(TAG, "accuracy changed: " + accuracy);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "Weather Station Started");
+        mSensorManager = getSystemService(SensorManager.class);
+        // Initialize LED strip
+        try {
+            mLedstrip = RainbowHat.openLedStrip();
+            mLedstrip.setBrightness(LEDSTRIP_BRIGHTNESS);
+            int[] colors = new int[7];
+            Arrays.fill(colors, Color.RED);
+            mLedstrip.write(colors);
+            // Because of a known APA102 issue, write the initial value twice.
+            mLedstrip.write(colors);
 
-        //TODO: Register peripheral drivers here
+            Log.d(TAG, "Initialized SPI LED strip");
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing LED strip", e);
+        }
+        // Initialize 7-segment display
+        try {
+            mDisplay = RainbowHat.openDisplay();
+            mDisplay.setEnabled(true);
+            mDisplay.display("1234");
+            Log.d(TAG, "Initialized I2C Display");
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing display", e);
+        }
+        // Initialize temperature/pressure sensors
+        try {
+            mEnvironmentalSensorDriver = RainbowHat.createSensorDriver();
+            // Register the drivers with the framework
+            mEnvironmentalSensorDriver.registerTemperatureSensor();
+            mEnvironmentalSensorDriver.registerPressureSensor();
+
+            Log.d(TAG, "Initialized I2C BMP280");
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing BMP280", e);
+        }
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        //TODO: Register for sensor events here
+        // Register the BMP280 temperature sensor
+        Sensor temperature = mSensorManager
+                .getDynamicSensorList(Sensor.TYPE_AMBIENT_TEMPERATURE).get(0);
+        mSensorManager.registerListener(mSensorEventListener, temperature,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        // Register the BMP280 pressure sensor
+        Sensor pressure = mSensorManager
+                .getDynamicSensorList(Sensor.TYPE_PRESSURE).get(0);
+        mSensorManager.registerListener(mSensorEventListener, pressure,
+                SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        //TODO: Unregister for sensor events here
+        mSensorManager.unregisterListener(mSensorEventListener);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (mEnvironmentalSensorDriver != null) {
+            try {
+                mEnvironmentalSensorDriver.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing sensors", e);
+            } finally {
+                mEnvironmentalSensorDriver = null;
+            }
+        }
 
-        //TODO: Close peripheral connections here
+        if (mDisplay != null) {
+            try {
+                mDisplay.clear();
+                mDisplay.setEnabled(false);
+                mDisplay.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing display", e);
+            } finally {
+                mDisplay = null;
+            }
+        }
+
+        if (mLedstrip != null) {
+            try {
+                mLedstrip.setBrightness(0);
+                mLedstrip.write(new int[7]);
+                mLedstrip.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing LED strip", e);
+            } finally {
+                mLedstrip = null;
+            }
+        }
     }
-
     /**
      * Update the 7-segment display with the latest temperature value.
      *
      * @param temperature Latest temperature value.
      */
     private void updateTemperatureDisplay(float temperature) {
-        //TODO: Add code to write a value to the segment display
+        if (mDisplay != null) {
+            try {
+                mDisplay.display(temperature);
+            } catch (IOException e) {
+                Log.e(TAG, "Error updating display", e);
+            }
+        }
     }
 
     /**
@@ -70,7 +184,14 @@ public class WeatherStationActivity extends Activity {
      * @param pressure Latest pressure value.
      */
     private void updateBarometerDisplay(float pressure) {
-        //TODO: Add code to send color data to the LED strip
+        if (mLedstrip != null) {
+            try {
+                int[] colors = RainbowUtil.getWeatherStripColors(pressure);
+                mLedstrip.write(colors);
+            } catch (IOException e) {
+                Log.e(TAG, "Error updating ledstrip", e);
+            }
+        }
     }
 
 }
